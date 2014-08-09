e059e618a809e375c36037c4d918cbd5df9f230f
Tomas Majer <tomasmajer@gmail.com>
2014-08-09 15:49:18 +0200

    First simple implementation

--- lib/daylimotion/player.rb
+++ lib/daylimotion/player.rb
@@ -7,13 +7,73 @@ module Daylimotion
     end
 
     class Player
-      def initialize video_id
+      @width = 512
+      @height = 360
+      @allow_fullscreen = true
+      @embed_url = nil
+
+      attr_accessor :logo, :autoplay
+
+      def initialize video_id = nil
         @video_id = video_id
       end
 
       def html
-        "todo"
+        attributes = build_attribues
+        allowfullscreen = @allow_fullscreen ? ' allowfullscreen' : ''
+        video_url = @video_id ? "http://www.dailymotion.com/embed/video/#{@video_id}" : @embed_url
+        "<iframe frameborder=\"0\" width=\"#{@width}\" height=\"#{@height}\" src=\"#{video_url}#{attributes}\"#{allowfullscreen}></iframe>"
       end
-	end
+
+      def embed_url embed_url
+        @embed_url = embed_url
+        @video_id = nil
+        self
+      end
+
+      def width width
+        @width = width
+        self
+      end
+
+      def height height
+        @height = height
+        self
+      end
+
+      def allowfullscreen
+        @allow_fullscreen = true
+      end
+
+      def disablefullscren
+        @allow_fullscreen = false
+      end
+
+      def prepare_attributes
+        result = {}
+        if defined? @autoplay
+          result[:autoplay] = @autoplay
+        end
+        if defined? @logo
+          result[:logo] = @logo
+        end
+        result
+      end
+
+      def build_attribues
+        attributes = prepare_attributes
+        if attributes.empty?
+          return ''
+        end
+
+        result = []
+        attributes.each do |key, value|
+          result << "#{key}=#{value}"
+        end
+
+        joined = result.join('&')
+        "?#{joined}"
+      end
+    end
   end
 end
