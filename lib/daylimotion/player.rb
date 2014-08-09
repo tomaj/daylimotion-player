118df14d7b9814d80cd7874ba0eb7b550ab34071
Tomas Majer <tomasmajer@gmail.com>
2014-08-09 15:44:50 +0200

    First test change

--- lib/daylimotion/player.rb
+++ lib/daylimotion/player.rb
@@ -2,6 +2,18 @@ require "daylimotion/player/version"
 
 module Daylimotion
   module Player
-    # Your code goes here...
+    def self.player video_id
+      Player.new video_id
+    end
+
+    class Player
+      def initialize video_id
+        @video_id = video_id
+      end
+
+      def html
+        "todo"
+      end
+	end
   end
 end
